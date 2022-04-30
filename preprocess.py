9754a623a599a763fa8c453727c44143d6fc6286
Whiteshark-314 <scientistyshock@gmail.com>
2022-04-30 10:14:58 +0530

    Cleanup Notebook and update preprocess function

--- preprocess.py
+++ preprocess.py
@@ -35,45 +35,37 @@ def preprocess():
     voxels = Path("models/models-binvox-solid/data")
     fimgs = [f for f in imgs.iterdir() if f.is_dir()]
     fvoxels = [f for f in voxels.iterdir() if f.is_file()]
+    fimgs_stem = [f.stem for f in imgs.iterdir() if f.is_dir()]
+    fvoxels_stem = [f.stem for f in voxels.iterdir() if f.is_file()]
     g = [0, 1, 6, 7]
+    diff = set(set(fvoxels_stem) - set(fimgs_stem))
 
     #To remove the files which do not have corresponding images or are unreadable or have zero volume
-    unreadable = 0
-    zerovolume = 0
-    i = -1
-    while i < len(fvoxels):
-        i += 1
-        try:
-            with open(str(fvoxels[i]), 'rb') as f:
-                model = binvox_rw.read_as_3d_array(f)
-        except:
-            unreadable += 1
-            print(str(fvoxels[i]), "Unreadable")
-            fvoxels[i].unlink()
-            if (i < len(fimgs)) and (fvoxels[i].stem == fimgs[i].stem):
-                fvoxels.pop(i)
-                fimgs.pop(i)
-            else:
-                fvoxels.pop(i)
-            continue
-        if np.sum(model.data) == 0:
-            zerovolume += 1
-            fvoxels[i].unlink()
-            if (i < len(fimgs)) and (fvoxels[i].stem == fimgs[i].stem):
-                fvoxels.pop(i)
-                fimgs.pop(i)
-            else:
-                fvoxels.pop(i)
-        else:
-            if i < len(fimgs) and i < len(fvoxels):
-                while not fvoxels[i].stem == fimgs[i].stem:
-                    fvoxels[i].unlink()
-                    fvoxels.pop(i)
-            else:
-                fvoxels[i].unlink()
-                fvoxels.pop(i)
+    if not len(fvoxels) == 11694:
+        unreadable = 0
+        zerovolume = 0
+        i = -1
+        remove = []
+        while i < len(fvoxels)-1:
+            i+=1
+            if fvoxels[i].stem in diff:
+                remove.append(fvoxels[i].stem)
+                continue
+            try:
+                with open(str(fvoxels[i]), 'rb') as f:
+                    model = binvox_rw.read_as_3d_array(f)
+                if np.sum(model.data) == 0:
+                    remove.append(fvoxels[i].stem)
+            except:
+                remove.append(fvoxels[i].stem)
+        print(f"Total unreadable:{unreadable}, Total 0volume:{zerovolume}")
 
-    print(f"Total unreadable:{unreadable}, Total 0volume:{zerovolume}")
+        for fvoxel in fvoxels:
+            if fvoxel.stem in remove:
+                fvoxel.unlink()
+        for fimg in fimgs:
+            if fimg.stem in remove.stem:
+                fimg.unlink()
 
     # To create 4 new folders, one for each view
     for ii in range(4):
@@ -81,6 +73,7 @@ def preprocess():
         Path(f"models/models-screenshots/view{ii}/data").mkdir(exist_ok=True)
 
     # To seperate each view of a model into it's corresponding image
+    fimgs = [f for f in imgs.iterdir() if f.is_dir()]
     for j in range(len(fimgs)):
         img_files = [f for f in fimgs[j].iterdir() if f.is_file]
         if len(img_files)==15:
