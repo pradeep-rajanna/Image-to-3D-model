07a004e64741ad187f4c6feaee5dd74a594ae764
pradeep-rajanna <pradeep.shac@gmail.com>
2022-05-11 01:06:07 +0530

    nothing

--- preprocess.py
+++ preprocess.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 
-from itertools import count
 import zipfile
 import binvox_rw
 import numpy as np
@@ -29,8 +28,8 @@ def preprocess():
     #         └── data
     #
 
-    imgs = Path("model/models-screenshots/screenshots")
-    voxels = Path("model/models-binvox-solid/models-binvox-solid")
+    imgs = Path("models/models-screenshots/screenshots")
+    voxels = Path("models/models-binvox-solid/models-binvox-solid")
     
     if not (imgs.parent.exists() and voxels.parent.exists()):
         pass   
@@ -45,11 +44,11 @@ def preprocess():
     fvoxels = [f for f in voxels.iterdir() if f.is_file()]
     fimgs_stem = [f.stem for f in imgs.iterdir() if f.is_dir()]
     fvoxels_stem = [f.stem for f in voxels.iterdir() if f.is_file()]
-    g = [0, 1, 6, 7, 2, 3, 4, 5, 8, 9, 10, 11, 12, 13]
+    g = [0, 1, 6, 7, 8, 9, 10, 11, 12, 13, 2, 3, 4, 5]
     diff = set(set(fvoxels_stem) - set(fimgs_stem))
 
     #To remove the files which do not have corresponding images or are unreadable or have zero volume
-    if not len(fvoxels) == 5:
+    if not len(fvoxels) == 11694:
         i = -1
         remove = []
         while i < len(fvoxels)-1:
@@ -65,11 +64,12 @@ def preprocess():
             except:
                 remove.append(fvoxels[i].stem)
         
-        Path(f"model/models-binvox-solid/data").mkdir(exist_ok=True)
-        
-        for i, fvoxel in enumerate(fvoxels):
-            if not fvoxel.stem in remove: 
-                fvoxel.rename(Path(f"model/models-binvox-solid/data"+"//"+ str(i)+".binvox"))
+        Path(f"models/models-binvox-solid/data").mkdir(exist_ok=True)
+        c = 0
+        for fvoxel in fvoxels:
+            if not fvoxel.stem in remove:
+                c += 1
+                fvoxel.rename(Path(f"models/models-binvox-solid/data/{c:05d}.binvox"))
         
         for fimg in fimgs:
             if fimg.stem in remove:
@@ -77,20 +77,21 @@ def preprocess():
 
     # To create 4 new folders, one for each view
     for ii in range(14):
-        Path(f"model/models-screenshots/view{ii}").mkdir(exist_ok=True)
-        Path(f"model/models-screenshots/view{ii}/data").mkdir(exist_ok=True)
+        Path(f"models/models-screenshots/view{ii}").mkdir(exist_ok=True)
+        Path(f"models/models-screenshots/view{ii}/data").mkdir(exist_ok=True)
 
     # To seperate each view of a model into it's corresponding image
     fimgs = [f for f in imgs.iterdir() if f.is_dir()]
+    c = 0
     for j in range(len(fimgs)):
         img_files = [f for f in fimgs[j].iterdir() if f.is_file]
-        print(len(img_files))
         if len(img_files)==15:
+            c += 1
             for k in range(14):
-                img_files[g[k]].rename(Path(f"model/models-screenshots/view{k}/data/{img_files[g[k]].name}"))
+                img_files[g[k]].rename(Path(f"models/models-screenshots/view{k}/data/{c:05d}-{k:02d}.png"))
     
-    rmtree(Path("model/models-screenshots/screenshots"))
-    rmtree(Path("model/models-binvox-solid/models-binvox-solid"))
+    rmtree(Path("models/models-screenshots/screenshots"))
+    rmtree(Path("models/models-binvox-solid/models-binvox-solid"))
 
 if __name__ == "__main__":
     preprocess()
